fb034aed4ec529e8e03c34f99e8be6736f245cdc
ies0411 <ies041196@gmail.com>
2022-07-09 13:48:30 +0000

    img

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,15 +1,24 @@
+#include <cv_bridge/cv_bridge.h>
+
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include <sstream>
 
 #include "ros/ros.h"
+#include "sensor_msgs/Image.h"
 #include "std_msgs/String.h"
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "talker");
     ros::NodeHandle n;
     ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1);
+    ros::Publisher img_pub = n.advertise<sensor_msgs::Image>("img", 10);
+    cv::Mat image = cv::imread("/root/img.jpg");
 
     ros::Rate loop_rate(10);
+
+    sensor_msgs::Image img_msgs;
+    cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg(img_msgs);
 
     // int count = 0;
     while (ros::ok()) {
@@ -20,6 +29,7 @@ int main(int argc, char** argv) {
         msg.data = "jiwook";
         // ROS_INFO("%s", msg.data.c_str());
         chatter_pub.publish(msg);
+        img_pub.publish(img_msgs);
         // ros::spinOnce();
         loop_rate.sleep();
         // ++count;
